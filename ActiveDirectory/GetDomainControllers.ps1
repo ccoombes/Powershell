108c3021057ae0f10bbdda50613e2d5ecc2fa30c
Coombes <chris.coombes@pmplimited.com.au>
2021-02-04 09:09:42 +1100

    Formating update

--- ActiveDirectory/GetDomainControllers.ps1
+++ ActiveDirectory/GetDomainControllers.ps1
@@ -8,7 +8,6 @@
   Creation Date:  03-03-2021
   Purpose/Change: Initial script development
 #>
-
 $domain = "abc.xyx"
 $AllDCs = Get-ADDomainController -Filter * -Server $domain 
 $AllDCs | Select-Object Hostname,Ipv4address,isglobalcatalog,site,forest,operatingsystem
